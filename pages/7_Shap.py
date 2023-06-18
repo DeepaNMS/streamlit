26c0f4a1327fb4039abc0da793e437c4102a9f1e
DeepaNMS <62841292+DeepaNMS@users.noreply.github.com>
2023-06-18 21:33:02 +0530

    Update 7_Shap.py

--- pages/7_Shap.py
+++ pages/7_Shap.py
@@ -15,6 +15,21 @@ st.set_page_config(
 
 st.markdown(f'<b><h0 style="color:#00008B;font-size:35px;">{"Model interpretation with SHAP:"}</h0><br>', unsafe_allow_html=True)
 
+# To set the background image of the page
+st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background-image: url("https://img.freepik.com/free-photo/abstract-luxury-gradient-blue-background-smooth-dark-blue-with-black-vignette-studio-banner_1258-63452.jpg?size=626&ext=jpg");
+             background-attachment: fixed;
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
+
+
 # To insert textual content 
 st.markdown(f'<p align="justify" font-family: "Times New Roman" style="color:#000000;"><br>{"Since the dataset is huge, the profiling has been done on the Test dataset."}</p><br>', unsafe_allow_html=True)
 
