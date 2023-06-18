cf29b859e99a5165059a954a1351749261ecc06d
DeepaNMS <62841292+DeepaNMS@users.noreply.github.com>
2023-06-18 23:44:14 +0530

    Update 7_SHAPInterpretation.py

--- pages/7_SHAPInterpretation.py
+++ pages/7_SHAPInterpretation.py
@@ -42,13 +42,12 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
 st.write('XGBoost model')
 model = xgboost.train({"learning_rate": 0.01}, xgboost.DMatrix(X, label=y), 100)
 
-st.markdown('''explain the model's predictions using SHAP''')
+st.markdown(f'<p align="justify" font-family: "Times New Roman" style="color:#000000;"><b>{"Explain the model predictions using SHAP:"}</b></p>', unsafe_allow_html=True)
     
 explainer = shap.TreeExplainer(model)
 shap_values = explainer.shap_values(X)
 
-st.write('<p style="font-size:130%"> #Visualize the first prediction explanation </p>', unsafe_allow_html=True)
+st.markdown(f'<p align="justify" font-family: "Times New Roman" style="color:#000000;"><b>{"Visualize the first prediction explanation:"}</b></p>', unsafe_allow_html=True)
 st_shap(shap.force_plot(explainer.expected_value, shap_values[0,:], X.iloc[0,:]))
-
-st.write('<p style="font-size:130%"> #Visualize the training set predictions </p>', unsafe_allow_html=True)
+st.markdown(f'<p align="justify" font-family: "Times New Roman" style="color:#000000;"><b>{"Visualize the predictions:"}</b></p>', unsafe_allow_html=True)
 st_shap(shap.force_plot(explainer.expected_value, shap_values, X), 400)
