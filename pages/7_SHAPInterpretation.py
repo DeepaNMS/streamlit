ae3c27b7e607b9a6ab3d8123dd7d572247da8c17
DeepaNMS <62841292+DeepaNMS@users.noreply.github.com>
2023-06-18 23:41:34 +0530

    Update 7_SHAPInterpretation.py

--- pages/7_SHAPInterpretation.py
+++ pages/7_SHAPInterpretation.py
@@ -6,9 +6,21 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
     
-#uploaded_file = st.file_uploader("Choose a file")
-#if uploaded_file is not None:
-#  df = pd.read_csv(uploaded_file)
+
+# To set the background image of the page
+st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background-image: url("https://img.freepik.com/free-photo/abstract-luxury-gradient-blue-background-smooth-dark-blue-with-black-vignette-studio-banner_1258-63452.jpg?size=626&ext=jpg");
+             background-attachment: fixed;
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
+
 
 @st.cache_data
 def load_data(url):
